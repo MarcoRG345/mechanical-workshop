006a8095f2b53fd735b2b4d30b078a472ac5fbdf
Marco Antonio <marco-raya@ciencias.unam.mx>
2025-01-31 02:16:02 -0600

    functionality changes_2

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ def create_app():
     app = Flask(__name__, static_folder = 'views', static_url_path='')
     app.debug = True
     app.secret_key = "key"
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://user:userpassword@mysql/mydatabase'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://user:userpassword@mysql:3306/mydatabase'
     CORS(app, resources={r"/auth/*": {"origins": "*", "methods": ["GET", "POST", "OPTIONS", "PUT"], "supports_credentials": True}})
     db.init_app(app)
     CORS(app)
