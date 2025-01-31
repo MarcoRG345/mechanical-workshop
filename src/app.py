b5adfb873c5bedb49627efb084b65dd9c0aad3aa
Marco Antonio <marco-raya@ciencias.unam.mx>
2025-01-31 03:18:16 -0600

    add database

--- src/app.py
+++ src/app.py
@@ -1,13 +1,22 @@
 from flask import Flask
 from flask_cors import CORS
 from models import db
-
+import os
 
 def create_app():
     app = Flask(__name__, static_folder = 'views', static_url_path='')
     app.debug = True
     app.secret_key = "key"
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://user:userpassword@mysql:3306/mydatabase'
+
+    # Obtener variables de entorno
+    DB_USER = os.getenv("DB_USER", "root")
+    DB_PASSWORD = os.getenv("DB_PASSWORD","dmFTemyrafxmfclzQiQrHkkoIdKJsdMY")
+    DB_HOST = os.getenv("DB_HOST" ,"viaduct.proxy.rlwy.net")
+    DB_PORT = os.getenv("DB_PORT", "16219")
+    DB_NAME = os.getenv("DB_NAME", "railway")
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
     CORS(app, resources={r"/auth/*": {"origins": "*", "methods": ["GET", "POST", "OPTIONS", "PUT"], "supports_credentials": True}})
     db.init_app(app)
     CORS(app)
